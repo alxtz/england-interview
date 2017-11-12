6112b2bbb72565162c48958e7d3dd49fc50c35e5
Alex Tzeng <alxtz.tw@gmail.com>
2017-11-12 15:22:26 +0800

    finish the working tape

--- front-end-code/src/pages/LandingPage/index.vue
+++ front-end-code/src/pages/LandingPage/index.vue
@@ -6,21 +6,106 @@
           <div class="ghostBlack">
           </div>
           <div class="payload">
-            <img
-              class="gameImg"
-              src="https://secure.mfortune.co.uk/cdn2/img//images/2822-60-vegas-vegas.png"
-              alt="payload"
-            />
-            <img
-              class="gameImg"
-              src="https://secure.mfortune.co.uk/cdn2/img//images/2822-60-vegas-vegas.png"
-              alt="payload"
-            />
-            <img
-              class="gameImg"
-              src="https://secure.mfortune.co.uk/cdn2/img//images/2822-60-vegas-vegas.png"
-              alt="payload"
-            />
+            <div class="tape">
+              <div
+                class="gridItem"
+                :style="{
+                  'background-image': `url(https://secure.mfortune.co.uk/cdn2/img//images/2822-60-vegas-vegas.png)`
+                }"
+              >
+              </div>
+              <div
+                class="gridItem"
+                :style="{
+                  'background-image': `url(https://secure.mfortune.co.uk/cdn2/img//images/4703-60-cat-and-mouse.png)`
+                }"
+              >
+              </div>
+              <div
+                class="gridItem"
+                :style="{
+                  'background-image': `url(https://secure.mfortune.co.uk/cdn2/img//images/4782-60-snakes-and-ladders.png)`
+                }"
+              >
+              </div>
+              <div
+                class="gridItem"
+                :style="{
+                  'background-image': `url(https://secure.mfortune.co.uk/cdn2/img//images/4689-60-burger-man.png)`
+                }"
+              >
+              </div>
+              <div
+                class="gridItem"
+                :style="{
+                  'background-image': `url(https://secure.mfortune.co.uk/cdn2/img//images/4764-60-which-witch.png)`
+                }"
+              >
+              </div>
+              <div
+                class="gridItem"
+                :style="{
+                  'background-image': `url(https://secure.mfortune.co.uk/cdn2/img//images/4697-60-buster-safe.png)`
+                }"
+              >
+              </div>
+              <div
+                class="gridItem"
+                :style="{
+                  'background-image': `url(https://secure.mfortune.co.uk/cdn2/img//images/2326-60-quest-for-fire.png)`
+                }"
+              >
+              </div>
+              <div
+                class="gridItem"
+                :style="{
+                  'background-image': `url(https://secure.mfortune.co.uk/cdn2/img//images/2822-60-vegas-vegas.png)`
+                }"
+              >
+              </div>
+              <div
+                class="gridItem"
+                :style="{
+                  'background-image': `url(https://secure.mfortune.co.uk/cdn2/img//images/4703-60-cat-and-mouse.png)`
+                }"
+              >
+              </div>
+              <div
+                class="gridItem"
+                :style="{
+                  'background-image': `url(https://secure.mfortune.co.uk/cdn2/img//images/4782-60-snakes-and-ladders.png)`
+                }"
+              >
+              </div>
+              <div
+                class="gridItem"
+                :style="{
+                  'background-image': `url(https://secure.mfortune.co.uk/cdn2/img//images/4689-60-burger-man.png)`
+                }"
+              >
+              </div>
+              <div
+                class="gridItem"
+                :style="{
+                  'background-image': `url(https://secure.mfortune.co.uk/cdn2/img//images/4764-60-which-witch.png)`
+                }"
+              >
+              </div>
+              <div
+                class="gridItem"
+                :style="{
+                  'background-image': `url(https://secure.mfortune.co.uk/cdn2/img//images/4697-60-buster-safe.png)`
+                }"
+              >
+              </div>
+              <div
+                class="gridItem"
+                :style="{
+                  'background-image': `url(https://secure.mfortune.co.uk/cdn2/img//images/2326-60-quest-for-fire.png)`
+                }"
+              >
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -29,21 +114,6 @@
           <div class="ghostBlack">
           </div>
           <div class="payload">
-            <img
-              class="gameImg"
-              src="https://secure.mfortune.co.uk/cdn2/img//images/2822-60-vegas-vegas.png"
-              alt="payload"
-            />
-            <img
-              class="gameImg"
-              src="https://secure.mfortune.co.uk/cdn2/img//images/2822-60-vegas-vegas.png"
-              alt="payload"
-            />
-            <img
-              class="gameImg"
-              src="https://secure.mfortune.co.uk/cdn2/img//images/2822-60-vegas-vegas.png"
-              alt="payload"
-            />
           </div>
         </div>
       </div>
@@ -53,32 +123,25 @@
 
           </div>
           <div class="payload">
-            <img
-              class="gameImg"
-              src="https://secure.mfortune.co.uk/cdn2/img//images/2822-60-vegas-vegas.png"
-              alt="payload"
-            />
-            <img
-              class="gameImg"
-              src="https://secure.mfortune.co.uk/cdn2/img//images/2822-60-vegas-vegas.png"
-              alt="payload"
-            />
-            <img
-              class="gameImg"
-              src="https://secure.mfortune.co.uk/cdn2/img//images/2822-60-vegas-vegas.png"
-              alt="payload"
-            />
+            <div
+              class="gridItem"
+              :style="{
+                'background-image': `url(https://secure.mfortune.co.uk/cdn2/img//images/2822-60-vegas-vegas.png)`
+              }"
+            >
+            </div>
           </div>
         </div>
       </div>
     </div>
-    <div class="list">
-
-    </div>
+    <!-- <ul class="list" v-for="slotItem in slotListFirst">
+      <li class="listItem">slotItem</li>
+    </ul> -->
   </div>
 </template>
 
 <script>
+  import _ from 'lodash'
   export default {
     name: 'LandingPage',
     data () {
@@ -101,8 +164,14 @@
           }
         )
         .then(
-          response => {
-            console.log(response)
+          list => {
+            console.log(list)
+            const shuffledFirst = _.shuffle(list)
+            this.slotListFirst = shuffledFirst.concat(shuffledFirst)
+            const shuffledSecond = _.shuffle(list)
+            this.slotListSecond = shuffledSecond.concat(shuffledSecond)
+            const shuffledThird = _.shuffle(list)
+            this.slotListThird = shuffledThird.concat(shuffledThird)
           },
           error => {
             console.log(error)
@@ -114,4 +183,5 @@
 
 <style scoped lang="scss">
   @import "./index.scss";
+  @import "./animation.scss";
 </style>
